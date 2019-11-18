72f040a080fb10bb10fb9215f575b3214d2ba517
steve-alex <steve.alex@hotmail.co.uk>
2019-11-18 15:32:06 +0000

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,28 @@
-// your Bomb code here!
+import React from 'react';
+import { prependOnceListener } from 'cluster';
+// import ReactDOM from 'react-dom';
+class Bomb extends React.Component {
+    constructor(props){
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    render(){
+        const displayMessage = () => {
+            if (this.state.secondsLeft === 0) {
+                return "Boom!"
+            } else {
+                return `${this.state.secondsLeft} seconds left before I go boom!`
+            }
+        }
+
+        return (
+            <h1>{displayMessage()}</h1>
+        )
+    }
+}
+
+
+export default Bomb;
