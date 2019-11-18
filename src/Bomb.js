1cb7ab980a1ea6e4f9598d93d2cb9850cc4543aa
Luke Prendergast <luke.prendergast94@gmail.com>
2019-11-18 16:34:07 +0000

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,24 @@
-// your Bomb code here!
+import React, { Component } from 'react'
+
+export default class Bomb extends Component {
+  constructor(props){
+    super()
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+  generateRatingElement = () => {
+    // debugger
+    if(this.props.initialCount > 0){
+    return  <div>{this.props.initialCount} seconds left before I go boom!</div>
+    }else{
+      return 'Boom!'
+    }
+  };
+
+  render(){
+    return(
+      <div>{this.generateRatingElement()}</div>
+    )
+  }
+}
